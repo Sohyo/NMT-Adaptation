17bb2a654fc7d9a1242f1c7feec8f06aba196a20
Sohyo <200924431Th!>
2020-12-14 15:32:28 +0100

    add datasets : tagged phrases(maximum lenth 4)

--- nmt_adaptation/post_process_phrases.py
+++ nmt_adaptation/post_process_phrases.py
@@ -1,7 +1,13 @@
-from nmt_adaptation.util import arr2txt
-
+from nmt_adaptation.util import arr2txt, text2arr
+import random
 
 def split_trg_src_phrase(path, save_name):
+    """
+    Split the phrase file into target and source
+    :param path: where the phrase files are
+    :param save_name: saving name. It will be added with '.de' and '.en'
+    :return:
+    """
     source = []
     target = []
     with open(path) as f:
@@ -14,13 +20,36 @@ def split_trg_src_phrase(path, save_name):
     arr2txt(target, "../data/edited_phrases/" + save_name + ".en")
 
 
+def edit_phrases(path, save_name, random_percentage):
+    #TODO
+    arr_phrase = text2arr(path)
+    selected = random.choices(arr_phrase, k=len(arr_phrase)*random_percentage)
+
+
+def add_tag(path, save_dir):
+    tag_added_phrases = []
+    phrases = text2arr(path)
+    for line in phrases:
+        tag_added_phrases.append("<PT> " + line + " </PT>")
+    arr2txt(tag_added_phrases, save_dir)
+
+
 def main():
     root_dir = "../data/extracted_phrases/"
+    save_dir = "../data/edited_phrases/"
     data_names = ["EMEA", "GNOME", "JRC"]
-    EMEA_path = "../data/extracted_phrases/EMEA_train_union"
+    languages = ["de", "en"]
+    # EMEA_path = "../data/extracted_phrases/EMEA_train_union"
 
-    for data_name in data_names:
-        split_trg_src_phrase(root_dir+data_name+"_train_union", data_name+"_train_phrase")
+    for data in data_names:
+        split_trg_src_phrase(root_dir+data+"_train_union_4", data+"_train_phrase_4")
+        for lang in languages:
+            add_tag(f'{save_dir}{data}_train_phrase_4.{lang}', f'{save_dir}{data}_tagged_train_phrase_4.{lang}')
+
+
+    # for data_name in data_names:
+    #     split_trg_src_phrase(root_dir+data_name+"_train_union", data_name+"_train_phrase")
+
 
 
 if __name__ == "__main__":
